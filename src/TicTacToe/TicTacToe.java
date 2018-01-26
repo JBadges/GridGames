14a7e2c44e48238b88ebe2169e10a1c7850f2462
kwkchang <kylechang@crescentschool.org>
2018-01-25 21:34:43 -0500

    End of game messages, bug where players tie instead of win, players are now referenced as colours

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -1,11 +1,13 @@
 package TicTacToe;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -15,6 +17,7 @@ public class TicTacToe extends Application {
 
     TicTacToeGrid grid;
     boolean isPlayerOneTurn = true;
+    int turnNumber = 1;
 
     public TicTacToe (Stage name) {
         start(name);
@@ -35,24 +38,59 @@ public class TicTacToe extends Application {
         root.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                if (grid.whoWon() == '\u0000') {
+                if (grid.whoWon() == "\u0000" && turnNumber <= 9) {
                     int[] hoveredLoc = grid.findHoveredSlot();
                     TicTacToePiece ticTacToePiece = grid.getCell(hoveredLoc[0], hoveredLoc[1]);
                     if (isPlayerOneTurn) {
                         if(grid.getPiece(hoveredLoc[0], hoveredLoc[1]) == '\u0000' ) {
-                            grid.setPiece(hoveredLoc[0], hoveredLoc[1], 'X');
+                            grid.setPiece(hoveredLoc[0], hoveredLoc[1], 'B');
                             isPlayerOneTurn = !isPlayerOneTurn;
+                            turnNumber++;
                         }
                     } else {
                         if(grid.getPiece(hoveredLoc[0], hoveredLoc[1]) == '\u0000' ) {
-                            grid.setPiece(hoveredLoc[0], hoveredLoc[1], 'O');
+                            grid.setPiece(hoveredLoc[0], hoveredLoc[1], 'Y');
                             isPlayerOneTurn = !isPlayerOneTurn;
+                            turnNumber++;
                         }
                     }
                     updateScreen(gc);
-                    if (grid.whoWon() != '\u0000') {
-                        System.out.println(grid.whoWon() + " WON YAY!");
+                    if (grid.whoWon() != "\u0000") {
+                        Button btnWin = new Button();
+                        btnWin.setPrefWidth(800);
+                        btnWin.setPrefHeight(800);
+                        btnWin.setText(grid.whoWon() + " WON YAY!");
+
+                        root.getChildren().add(btnWin);
+
+
+                        btnWin.setOnAction(new EventHandler<ActionEvent>() {
+
+                            public void handle(ActionEvent event) {
+
+                                System.exit(0);                            }
+                        });
+
                     }
+
+                    if (turnNumber == 10 && grid.whoWon() == "\u0000") {
+                        Button btnTie = new Button();
+                        btnTie.setPrefWidth(800);
+                        btnTie.setPrefHeight(800);
+                        btnTie.setText("TIE GAME!");
+
+                        root.getChildren().add(btnTie);
+
+
+                        btnTie.setOnAction(new EventHandler<ActionEvent>() {
+
+                            public void handle(ActionEvent event) {
+
+                                System.exit(0);                            }
+                        });
+
+                    }
+
                 }
             }
         });
@@ -63,7 +101,7 @@ public class TicTacToe extends Application {
         for (int row = 0; row < 3; row++) {
             for (int col = 0; col < 3; col++) {
                 TicTacToePiece tttp = grid.getCell(row, col);
-                gc.setFill(grid.getPiece(row, col) == 'X' ? Color.BLACK : grid.getPiece(row, col) == 'O' ? Color.YELLOW : Color.GRAY);
+                gc.setFill(grid.getPiece(row, col) == 'B' ? Color.BLACK : grid.getPiece(row, col) == 'Y' ? Color.YELLOW : Color.GRAY);
                 gc.fillRect(tttp.getCoord().getX(), tttp.getCoord().getY(), tttp.getSize(), tttp.getSize());
                 gc.setStroke(Color.WHITE);
                 gc.strokeRect(tttp.getCoord().getX(), tttp.getCoord().getY(), tttp.getSize(), tttp.getSize());
