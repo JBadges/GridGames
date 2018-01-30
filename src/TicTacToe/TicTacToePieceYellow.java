a941272d2ed01bb4ea66cb8eee5dc2679d6b649f
kwkchang <kylechang@crescentschool.org>
2018-01-30 17:29:32 -0500

    comments

--- src/TicTacToe/TicTacToePieceYellow.java
+++ src/TicTacToe/TicTacToePieceYellow.java
@@ -4,13 +4,15 @@ import javafx.stage.Stage;
 
 import java.awt.geom.Point2D;
 
-
+//Concrete class that inherits from the Tic Tac Toe Piece abstract class.
 public class TicTacToePieceYellow extends TicTacToePiece {
 
+    //Uses the keyword super to call the constructor of the abstract class.
     public TicTacToePieceYellow(Stage stage, Point2D coord, double size) {
         super(stage, coord, size);
     }
 
+    //Overrides the method from its superclass and returns 'Y' for black.
     @Override
     public char getPieceType() {
         return 'Y';
